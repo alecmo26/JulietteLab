f12d795d3d7f6c9c9862b4cecde4af68dede4b13
alecmo26 <111534268+alecmo26@users.noreply.github.com>
2022-12-03 18:15:49 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  console.log("doc is ready");
+
+});
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+function card_open() {
+  document.getElementById("card").style.display = "block";
+}
